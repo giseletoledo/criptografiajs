accd4cacf0212286d1383e7c39d6bd61c78f6b97
gisele <gisesonia@gmail.com>
2024-07-08 19:29:30 -0300

    refatorando o javasrcript

--- app.js
+++ app.js
@@ -1,61 +1,50 @@
+const inputTextElement = document.getElementById('inputText');
+const resultMessage = document.getElementById('resultMessage');
+const noMessageImage = document.getElementById('noMessageImage');
+const outputText = document.getElementById('outputText');
+const decryptBtn = document.getElementById('decryptBtn');
+
 function checkInput() {
-    const inputText = document.getElementById('inputText').value;
-    const resultMessage = document.getElementById('resultMessage');
-    const noMessageImage = document.getElementById('noMessageImage');
-    const outputText = document.getElementById('outputText');
+    const inputText = inputTextElement.value;
 
     if (inputText === '') {
         resultMessage.textContent = 'Nenhuma mensagem encontrada';
         noMessageImage.style.display = 'block';
         outputText.textContent = 'Digite um texto que você deseja criptografar ou descriptografar.';
-        decryptBtn.disabled = true; 
+        decryptBtn.disabled = true;
     } else {
         resultMessage.textContent = '';
         noMessageImage.style.display = 'none';
+        decryptBtn.disabled = false;
     }
 }
 
 function encryptText() {
-    const inputText = document.getElementById('inputText').value;
-    const outputText = document.getElementById('outputText');
-    const resultMessage = document.getElementById('resultMessage');
-    const decryptBtn = document.getElementById('decryptBtn');
+    const inputText = inputTextElement.value;
 
-    if (inputText === '') {
-        resultMessage.textContent = 'Favor digitar um texto';
-        return
-    }
-
-     // Verificar se contém apenas letras minúsculas sem acentos
-     if (/^[a-z]*$/.test(inputText)) {
+    if (/^[a-z]*$/.test(inputText)) {
         const encryptedText = transformText(inputText, 'encrypt');
         outputText.textContent = encryptedText;
-        decryptBtn.disabled = false;
         resultMessage.textContent = 'Texto criptografado com sucesso!';
     } else {
-        resultMessage.textContent = 'Apenas letras minúsculas sem acentos são permitidas!';
-        outputText.textContent = 'Texto descriptografado aparecerá aqui...';
+        checkInput()
     }
 }
 
 function decryptText() {
-    const outputText = document.getElementById('outputText').textContent;
-    const resultMessage = document.getElementById('resultMessage');
+    const inputText = outputText.textContent;
 
-    if (outputText) {
-        const decryptedText = transformText(outputText, 'decrypt');
-        document.getElementById('outputText').textContent = decryptedText;
+    if (inputText) {
+        const decryptedText = transformText(inputText, 'decrypt');
+        outputText.textContent = decryptedText;
         resultMessage.textContent = 'Texto descriptografado com sucesso!';
     } else {
-        resultMessage.textContent = 'Nenhuma mensagem encontrada';
+        checkInput()
     }
 }
 
 function copyText() {
-    const outputText = document.getElementById('outputText').textContent;
-    const resultMessage = document.getElementById('resultMessage');
-
-    navigator.clipboard.writeText(outputText).then(() => {
+    navigator.clipboard.writeText(outputText.textContent).then(() => {
         resultMessage.textContent = 'Texto copiado para a área de transferência';
     });
 }
@@ -85,3 +74,10 @@ function transformText(text, mode) {
             });
     }
 }
+
+// Verificar a entrada quando o usuário digitar
+inputTextElement.addEventListener('input', checkInput);
+
+// Inicialização
+checkInput();
+
